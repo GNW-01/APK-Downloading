8c4f9324f4255a61ed45933b5056f9a8cdc5adaf
JyanMan <196519339+JyanMan@users.noreply.github.com.>
2025-03-15 17:41:43 +0800

    add icon, remove home icons

---
+++ Contents/Icon.js
@@ -0,0 +1,12 @@
+class Icon {
+    constructor(label, content) {
+        this.label = label;
+        this.icon = document.getElementById(this.label);
+        this.content = content;
+
+        this.icon.addEventListener('click', () => {
+            
+        })
+    }
+
+}
