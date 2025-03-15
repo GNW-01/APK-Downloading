011095ec706e748c52e456050ebf703a65545a10
JyanMan <196519339+JyanMan@users.noreply.github.com.>
2025-03-15 17:02:04 +0800

    made home tab as default

--- Contents/main.js
+++ Contents/main.js
@@ -1,9 +1,12 @@
 import Home from "./home.js";
 import Guide from "./guide.js";
 import Information from "./Information.js";
+import { setTab } from "./buttons.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     new Home();
     new Guide();
     new Information();
 });
+
+setTab("home");
