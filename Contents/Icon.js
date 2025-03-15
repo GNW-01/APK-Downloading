3b81983466e121ea01e1268006c568ac5c185ee4
JyanMan <196519339+JyanMan@users.noreply.github.com.>
2025-03-15 17:49:01 +0800

    add click event to all icons

--- Contents/Icon.js
+++ Contents/Icon.js
@@ -1,12 +1,21 @@
-class Icon {
+const icons = document.querySelectorAll(".icon");
+
+class IconManager {
     constructor(label, content) {
         this.label = label;
         this.icon = document.getElementById(this.label);
         this.content = content;
-
-        this.icon.addEventListener('click', () => {
-            
-        })
+        
+        this.addClickEvent();
     }
 
+    addClickEvent() {
+        for (let i = 0; i < icons.length; i++) {
+            icons[i].addEventListener('click', () => {
+                console.log(icons[i]);
+            })
+        }
+    }
 }
+
+export default IconManager;
--- Contents/main.js
+++ Contents/main.js
@@ -2,6 +2,7 @@ import Home from "./Home.js";
 import Guide from "./Guide.js";
 import Information from "./Information.js";
 import { setTab } from "./Buttons.js";
+import IconManager from "./Icon.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     new Home();
@@ -11,3 +12,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //set the home tab as the default tab
 setTab("home");
+const iconManager = new IconManager();
