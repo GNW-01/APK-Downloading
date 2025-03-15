52b2f45eb5412497ec96d6f0b4ff220577ed6445
GNW-01 <195465166+GNW-01@users.noreply.github.com>
2025-03-15 15:26:41 +0800

    c

--- Contents/main.js
+++ Contents/main.js
@@ -1,5 +1,7 @@
 const homeBtn = document.getElementById("home-btn");
-
+import Home from './Home.js';
+import Guide from './Guide.js';
+import Information from './Information.js';
 homeBtn.onclick = function() {
     console.log("hello wordl");
 }
